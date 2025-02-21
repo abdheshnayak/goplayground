6d60a1aef8490788ee901552722f51fa9e595b1f
Abdhesh Nayak <abdheshnayak@gmail.com>
2025-02-21 23:40:44 +0530

    :bug: Added recover to handle runtime crash

--- main.go
+++ main.go
@@ -10,24 +10,31 @@ import (
 )
 
 func runGoCode(this js.Value, args []js.Value) interface{} {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in runGoCode:", r)
+		}
+	}()
+
 	code := args[0].String()
 	input := args[1].String()
 	inputReader := bytes.NewReader([]byte(input))
 
 	var out bytes.Buffer
-	var err bytes.Buffer
+	// var err bytes.Buffer
 
 	i := interp.New(interp.Options{
-		Stdin:        inputReader,
-		Stdout:       &out,
-		Stderr:       &err,
-		Unrestricted: false,
+		Stdin:                inputReader,
+		Stdout:               &out,
+		Unrestricted:         false,
 	})
 	i.Use(stdlib.Symbols)
+
 
 	if _, err := i.Eval(code); err != nil {
 		return fmt.Sprintf("Error: %v", err)
 	}
+
 
 	return out.String()
 }
